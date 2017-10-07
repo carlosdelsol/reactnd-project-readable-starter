7b5a3131c5967fb657bc60c52899273f8ade7cea
Carlos del Sol <carlosdelsol@MacBook-Pro-de-Carlos.local>
2017-10-07 03:55:40 +0200

    Added a basic redux scaffold

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,37 @@
+export const ADD_NEW_POSTS = 'ADD_NEW_POSTS'
+export const ADD_COMMENTS = 'ADD_COMMENTS'
+export const EDIT_POSTS = 'EDIT_POSTS'
+export const EDIT_COMMENTS = 'EDIT_COMMENTS'
+export const DELETE_POSTS = 'DELETE_POSTS'
+export const DELETE_COMMENTS = 'DELETE_COMMENTS'
+
+export function addNewPosts ({}){
+  return{
+    type: ADD_NEW_POSTS, 
+  }
+}
+export function addComments ({}){
+  return{
+    type: ADD_COMMENTS, 
+  }
+}
+export function editPosts ({}){
+  return{
+    type: EDIT_POSTS, 
+  }
+}
+export function editComments ({}){
+  return{
+    type: EDIT_COMMENTS, 
+  }
+}
+export function deletePosts ({}){
+  return{
+    type: DELETE_POSTS, 
+  }
+}
+export function deleteComments ({}){
+  return{
+    type: DELETE_COMMENTS, 
+  }
+}
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,50 @@
+import { combineReducers } from 'redux'
+
+import {
+  ADD_NEW_POSTS,
+  ADD_COMMENTS,
+  EDIT_POSTS,
+  EDIT_COMMENTS,
+  DELETE_POSTS,
+  DELETE_COMMENTS,
+} from '../actions'
+
+
+function Categories (state = {}, action) {
+  switch (action.type) {
+    default :
+      return state
+  }
+}
+
+function Posts (state = initialCalendarState, action) {
+  switch (action.type) {
+    case ADD_NEW_POSTS :
+      return state
+    case EDIT_POSTS :
+      return state
+    case DELETE_POSTS :
+      return state
+    default :
+      return state
+  }
+}
+
+function Comments (state = initialCalendarState, action) {
+  switch (action.type) {
+    case ADD_COMMENTS :
+      return state
+    case EDIT_COMMENTS :
+      return state
+    case DELETE_COMMENTS :
+      return state
+    default :
+      return state
+  }
+}
+
+export default combineReducers({
+  Categories,
+  Posts,
+  Comments,
+})
