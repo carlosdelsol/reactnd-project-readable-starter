737b79a8632ee3317fcb60d070cc205c6801d753
Carlos del Sol <carlosdelsol@MacBook-Pro-de-Carlos.local>
2017-10-12 02:16:03 +0200

    Upated Home with icon

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HomeIcon from 'react-icons/lib/ti/home'
 
 const NavBar = () => (
     <nav className="navbar navbar-default">
         <div className="container">
         <div className="navbar-header">
-            <Link className="navbar-brand" to="/">Home</Link>
+            <Link className="navbar-brand" to="/"><HomeIcon size={30}/></Link>
             </div>
         </div>
     </nav>
